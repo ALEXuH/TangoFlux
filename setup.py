fb5b2abcadfc4bb4c43e151669b02b04be6d6b85
Alexuh <32250185+ALEXuH@users.noreply.github.com>
2025-09-25 12:12:04 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     install_requires=[
         "torch",
         "torchaudio",
-        "torchlibrosa=",
+        "torchlibrosa",
         "torchvision",
         "transformers",
         "diffusers",
